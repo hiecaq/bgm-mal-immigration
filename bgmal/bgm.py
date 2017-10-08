6dcd740c405cd7cf721a2ecccf72c63df708b05e
quinoa42 <quinoa42@outlook.jp>
2017-10-08 14:06:18 -0700

    finish bgm module

---
+++ bgmal/bgm.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+    bgm_mal_immigration.bgm
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    Deal with the bangumi APIs
+
+    :copyright: (c) 2017 by quinoa42.
+    :license: MIT, see LICENSE for more details.
+"""
+
+import logging
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+
+
+class Bangumi(object):
+    """Manipulate the bgm api"""
+
+    def __init__(self, email, password):
+        """Construct a bangumi object with given email and password
+
+        :param str email: user's email
+        :param str password: user's password
+        :returns: a ``Bangumi`` object
+
+        """
+        data = {
+            'password': password,
+            'username': email,
+            'auth': 0,
+            'sysuid': 0,
+            'sysusername': 0
+        }
+        url = 'https://api.bgm.tv/auth?source=onAir'
+        r = requests.post(url, data=data)
+        r.raise_for_status()
+
+        output = r.json()
+
+        try:
+            self._uid = output['username']
+            self._auth = output['auth']
+        except Exception as e:
+            logger.error("logging failed: %s", e)
+            raise e
+
+    def watched_list(self):
+        """Return the watched list of anime
+
+        :returns: a list of anime entries, described in dict of name and score
+
+        """
+        url = 'https://bgm.tv/anime/list/{0}/collect'.format(self._uid)
+        page = 1
+        watched = []
+        while True:
+            r = requests.get(url, params={'page': page})
+            r.raise_for_status()
+            soup = BeautifulSoup(r.content, 'lxml')
+            items = soup.find_all('li', class_='item')
+            for item in items:
+                anime = Bgmanime(item)
+                watched.append({'title': anime.title, 'score': anime.score})
+            if not Bangumi._has_next_page(soup):
+                break
+            page += 1
+            time.sleep(1)
+
+        return watched
+
+    @classmethod
+    def _has_next_page(cls, soup):
+        """Return if this page have a next page.
+
+        :param BeautifulSoup soup: the soup of the given page
+        :returns: True or False
+
+        """
+        pages = soup.find(class_='page_inner')
+        return pages.find_all(class_='p')[-1].string == '››'
+
+
+class Bgmanime(object):
+    """One entry for one anime from Bangumi"""
+
+    def __init__(self, item):
+        """
+
+        :type tag: bs4.element.Tag
+        :returns: a ``Bgmanime`` object
+
+        """
+        self._item = item
+
+    @property
+    def score(self):
+        """Return the user's score of this anime
+        :returns: score
+        :rtype: int
+
+        """
+        starsinfo = self._item.find(class_='starsinfo')['class']
+        stars = (
+            starsinfo[0] if starsinfo[0] != 'starsinfo' else starsinfo[-1]
+        )
+        return int(stars[6:])
+
+    @property
+    def title(self):
+        """Return the title of this anime
+        :returns: title of this anime
+        :rtype: str
+
+        """
+        return (
+            self._item.find(class_='grey').string
+            if self._item.find(class_='grey') is not None else
+            self._item.find(class_='l').string
+        )
